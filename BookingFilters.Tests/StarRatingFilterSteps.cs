79e3ca69349ca00f0cc1f062dc587b97b1b31637
gemmamary <gemmamarymcgee@gmail.com>
2020-08-17 22:49:40 +0100

    added outlines for given methods

--- BookingFilters.Tests/StarRatingFilterSteps.cs
+++ BookingFilters.Tests/StarRatingFilterSteps.cs
@@ -1,14 +1,38 @@
 using System;
-using Xunit;
+using TechTalk.SpecFlow;
 
 namespace BookingFilters.Tests
 {
-    public class UnitTest1
+    public class StarRatingFilterSteps
     {
-        [Fact]
-        public void Test1()
+        [Given(@"I am on the booking.com website")]
+        public void GivenIAmOnTheBookingWebsiteHomePage()
         {
+            ScenarioContext.Current.Pending();
+        }
 
+        [Given(@"I select a destination")]
+        public void GivenISelectDestination()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I select a check in date")]
+        public void GivenISelectCheckInDate()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I select a check out date")]
+        public void GivenISelectCheckOutDate()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I submit my booking details")]
+        public void GivenISubmitBookingDetails()
+        {
+            ScenarioContext.Current.Pending();
         }
     }
 }
