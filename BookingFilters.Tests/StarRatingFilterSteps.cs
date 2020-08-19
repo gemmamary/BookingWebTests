92a0d6cf1d03bcbe5789cd0fca6f0edd716e0b27
gemmamary <gemmamarymcgee@gmail.com>
2020-08-19 03:04:07 +0100

    added method for accepting cookies

--- BookingFilters.Tests/StarRatingFilterSteps.cs
+++ BookingFilters.Tests/StarRatingFilterSteps.cs
@@ -16,7 +16,9 @@ namespace BookingFilters.Tests
         public void GivenIAmOnTheBooking_ComWebsite()
         {
             _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
             _driver.Navigate().GoToUrl("https://www.booking.com/");
+            _driver.FindElement(By.XPath("//button[@data-gdpr-consent=\"accept\"]")).Click();
         }
         
         [Given(@"I select a destination")]
@@ -41,8 +43,8 @@ namespace BookingFilters.Tests
             var selectCheckoutMonth = new SelectElement(_driver.FindElement(By.XPath("(//div[@class=\"sb-date-field__select -month-year js-date-field__part\"])[2]//select")));
             var selectCheckoutDay = new SelectElement(_driver.FindElement(By.XPath("//select[@name=\"checkout_monthday\"]")));
 
-            selectCheckoutMonth.SelectByText("November 2020");
-            selectCheckoutDay.SelectByText("15, Sunday");
+            selectCheckoutMonth.SelectByValue("10-2020");
+            selectCheckoutDay.SelectByValue("8");
         }
         
         [Given(@"I submit my booking details")]
@@ -51,22 +53,23 @@ namespace BookingFilters.Tests
             _driver.FindElement(By.XPath("//button[@data-sb-id=\"main\"]")).Click();
         }
         
-        [When(@"I filter the results by a star rating of (.*) stars")]
-        public void WhenIFilterTheResultsByAStarRatingOfStars(int p0)
+        [When(@"I filter the results by a star rating of 3 stars")]
+        public void WhenIFilterTheResultsByAStarRatingOf3Stars()
         {
-            _driver.FindElement(By.XPath("//a[@data-id=\"class-3\"]//label//input")).Click();
+            _driver.FindElement(By.XPath("//a[@data-id=\"class-3\"]//label")).Click();
         }
         
-        [Then(@"My results contain only (.*) star hotels")]
-        public void ThenMyResultsContainOnlyStarHotels(int p0)
+        [Then(@"My results contain only 3 star hotels")]
+        public void ThenMyResultsContainOnly3StarHotels()
         {
             var results = _driver.FindElements(By.Id("hotellist_inner"));
         }
-
+        
         [AfterScenario]
         public void DisposeWebDriver()
         {
             _driver.Dispose();
         }
+        
     }
 }
